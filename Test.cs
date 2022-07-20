7f2e05ac39ca024d1518c5158a43c832b91890be
Marek Slowinski <mslowinski@antmicro.com>
2022-07-20 16:54:51 +0200

    Add minimal reproducible example

---
+++ Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void ShouldPass()
+        {
+        }
+    }
+}
+
